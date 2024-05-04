101a66da6b0f6b11823761333efe43b024a1149b
zhy2on <dhwlgus5940@gmail.com>
2024-05-04 20:21:46 +0900

    feat: websocket handler에서 클라이언트의 패킷 캡쳐 시작 여부 메시지를 받도록 구현

--- wavnes/websocket/websocket_handler.py
+++ wavnes/websocket/websocket_handler.py
@@ -1,4 +1,5 @@
 import asyncio
+import json
 from ..capture import start_sniffer
 
 
@@ -8,12 +9,20 @@ async def websocket_handler(websocket, path):
     print(f"Client connected from {client_addr}")
     print("Starting packet capture...")
 
-    # MQTT 패킷 스니퍼 시작
-    sniffer_task = asyncio.create_task(start_sniffer(websocket))
-
     try:
-        await sniffer_task
+        async for message in websocket:
+            print(f"Received message from client: {message}")
+            data = json.loads(message)
+            if data.get("type") == "start_capture":
+                print("Starting packet capture...")
+                # MQTT 패킷 스니퍼 시작
+                sniffer_task = asyncio.create_task(start_sniffer(websocket))
+
+                try:
+                    await sniffer_task
+                finally:
+                    # 클라이언트 연결 종료 시 태스크 취소
+                    sniffer_task.cancel()
+
     finally:
-        # 클라이언트 연결 종료 시 태스크 취소
-        sniffer_task.cancel()
         print(f"Client disconnected from {client_addr}")
