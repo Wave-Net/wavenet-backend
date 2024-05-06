72cfc617cebfcedbcdffaf47c2633f6abc8dd8de
zhy2on <dhwlgus5940@gmail.com>
2024-05-06 14:34:22 +0900

    feat: 웹소켓에서 start_capture 메시지를 받았을 때 패킷 캡쳐가 시작되도록 구현

--- wavnes/websocket/websocket_handler.py
+++ wavnes/websocket/websocket_handler.py
@@ -7,7 +7,6 @@ async def websocket_handler(websocket, path):
     client_addr = websocket.remote_address
 
     print(f"Client connected from {client_addr}")
-    print("Starting packet capture...")
 
     try:
         async for message in websocket:
@@ -17,11 +16,11 @@ async def websocket_handler(websocket, path):
                 print("Starting packet capture...")
                 # MQTT 패킷 스니퍼 시작
                 sniffer_task = asyncio.create_task(start_sniffer(websocket))
-
                 try:
                     await sniffer_task
                 finally:
                     # 클라이언트 연결 종료 시 태스크 취소
+                    print("Stop packet capture...")
                     sniffer_task.cancel()
 
     finally:
