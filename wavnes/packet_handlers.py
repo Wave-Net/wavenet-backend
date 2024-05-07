744ca9322e069f720273e6313bef8e8f751ba782
zhy2on <dhwlgus5940@gmail.com>
2024-05-07 19:55:14 +0900

    feat: 프론트에서 요구하는 사항을 만족하도록 MQTTHandler 수정

--- wavnes/packet_handlers.py
+++ wavnes/packet_handlers.py
@@ -33,13 +33,62 @@ class MQTTHandler(PacketHandler):
     def process_packet(self, packet):
         mqtt_packet = packet[MQTT]
 
-        if hasattr(mqtt_packet, 'flags'):
-            self.packet_info['flags'] = int(mqtt_packet.flags)
+        self.packet_info.update({
+            'name': 'MQTT',
+            'mqtt_type': str(type(mqtt_packet).__name__),
+            'mqtt_dup': int(mqtt_packet.DUP),
+            'mqtt_qos': int(mqtt_packet.QOS),
+            'mqtt_retain': int(mqtt_packet.RETAIN),
+        })
 
-        if hasattr(mqtt_packet, 'topic'):
-            self.packet_info['topic'] = str(mqtt_packet.topic)
+        if isinstance(mqtt_packet, MQTTConnect):
+            self.packet_info.update({
+                'connect_proto_name': str(mqtt_packet.protoname),
+                'connect_mqtt_level': f'v{mqtt_packet.protolevel}',
+                'connect_usernameflag': int(mqtt_packet.usernameflag),
+                'connect_passwordflag': int(mqtt_packet.passwordflag),
+                'connect_willretainflag': int(mqtt_packet.willretainflag),
+                'connect_willQOSflag': int(mqtt_packet.willQOSflag),
+                'connect_willflag': int(mqtt_packet.willflag),
+                'connect_cleansession': int(mqtt_packet.cleansession),
+                'connect_reserved': int(mqtt_packet.reserved),
+                'connect_keep_alive': int(mqtt_packet.klive),
+                'connect_clientId': str(mqtt_packet.clientId),
+            })
+            if mqtt_packet.willflag:
+                self.packet_info['connect_willtopic'] = str(
+                    mqtt_packet.willtopic)
+                self.packet_info['connect_willmsg'] = str(mqtt_packet.willmsg)
+            if mqtt_packet.usernameflag:
+                self.packet_info['connect_username'] = str(
+                    mqtt_packet.username)
+            if mqtt_packet.passwordflag:
+                self.packet_info['connect_password'] = str(
+                    mqtt_packet.password)
 
-        if hasattr(mqtt_packet, 'value'):
-            self.packet_info['value'] = mqtt_packet.value.decode('utf-8')
+        elif isinstance(mqtt_packet, MQTTConnack):
+            self.packet_info.update({
+                'connack_ackflag': int(mqtt_packet.sesspresent),
+                'connack_return_code': str(mqtt_packet.retcode),
+            })
+
+        elif isinstance(mqtt_packet, MQTTPublish):
+            self.packet_info.update({
+                'publish_topic': str(mqtt_packet.topic),
+                'publish_msgid': str(mqtt_packet.msgid),
+                'publish_msgvalue': str(mqtt_packet.value),
+            })
+
+        elif isinstance(mqtt_packet, MQTTPuback):
+            self.packet_info['puback_msgid'] = str(mqtt_packet.msgid)
+
+        elif isinstance(mqtt_packet, MQTTPubrec):
+            self.packet_info['pubrec_msgid'] = str(mqtt_packet.msgid)
+
+        elif isinstance(mqtt_packet, MQTTPubrel):
+            self.packet_info['pubrel_msgid'] = str(mqtt_packet.msgid)
+
+        elif isinstance(mqtt_packet, MQTTPubcomp):
+            self.packet_info['pubcomp_msgid'] = str(mqtt_packet.msgid)
 
         return self.packet_info
