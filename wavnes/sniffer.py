bdf1f201612ad713a03de86f143f90a9fc23e8e3
zhy2on <dhwlgus5940@gmail.com>
2024-05-08 15:59:47 +0900

    feat: 스니퍼 시작 후 1초에 한 번씩 클라이언트에 패킷 통계 보내도록 추가

--- wavnes/sniffer.py
+++ wavnes/sniffer.py
@@ -20,9 +20,28 @@ class Sniffer:
         self.sniffer_thread = None
         self.lock = threading.Lock()
         self.is_running = False
+        self.target_ip = '172.16.17.113'
+        self.packet_statics = {
+            'send_pkt': 0,
+            'recv_pkt': 0,
+            'send_data': 0,
+            'recv_data': 0,
+        }
+        self.previous_statics = None
 
     def _update_previous_time(self, packet):
         self.previous_time = packet.time
+
+    def _update_packet_statics(self, packet_info):
+        with self.lock:
+            if packet_info.get('source_ip') == self.target_ip:
+                self.packet_statics['send_pkt'] += 1
+                self.packet_statics['send_data'] += packet_info.get(
+                    'length', 0)
+            elif packet_info.get('destination_ip') == self.target_ip:
+                self.packet_statics['recv_pkt'] += 1
+                self.packet_statics['recv_data'] += packet_info.get(
+                    'length', 0)
 
     def _packet_callback(self, packet, websocket, loop):
         handler = _select_packet_handler(packet)
@@ -34,6 +53,7 @@ class Sniffer:
         packet_info.update(packet_time_info(
             self.start_time, self.previous_time, packet))
         self._update_previous_time(packet)
+        self._update_packet_statics(packet_info)
 
         json_data = json.dumps(packet_info)
         asyncio.run_coroutine_threadsafe(websocket.send(json_data), loop)
@@ -57,8 +77,32 @@ class Sniffer:
         self.sniffer_thread = threading.Thread(target=sniff_thread)
         self.sniffer_thread.start()
 
+        self.previous_statics = self.packet_statics.copy()
+        await self.send_packet_statics()
+
     def stop_sniff(self):
         if self.sniffer_thread and self.sniffer_thread.is_alive():
             with self.lock:
                 self.is_running = False
             self.sniffer_thread.join()
+
+    async def send_packet_statics(self):
+        while True:
+            await asyncio.sleep(1)
+            with self.lock:
+                total_statics = self.packet_statics.copy()
+                if self.previous_statics:
+                    statics_delta = {
+                        'send_pkt': total_statics['send_pkt'] - self.previous_statics['send_pkt'],
+                        'recv_pkt': total_statics['recv_pkt'] - self.previous_statics['recv_pkt'],
+                        'send_data': total_statics['send_data'] - self.previous_statics['send_data'],
+                        'recv_data': total_statics['recv_data'] - self.previous_statics['recv_data'],
+                    }
+                    self.previous_statics = total_statics.copy()
+                else:
+                    statics_delta = total_statics
+
+                await self.websocket.send(json.dumps({
+                    'total_statics': total_statics,
+                    'statics_delta': statics_delta
+                }))
